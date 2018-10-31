fd25c7a7a6a13322950c648f0ca701699aae7c90
Allan <kalnayallan@gmail.com>
2018-10-31 23:42:22 +0100

    added database support
    Added a file for creating stations in db. Now inserting scraped data into database.
    
    Signed-off-by: Allan <kalnayallan@gmail.com>

--- stations_create.py
+++ stations_create.py
@@ -6,9 +6,9 @@ import requests
 from bs4 import BeautifulSoup
 
 INSERT_BIKES = (
-    "INSERT INTO bikes "
-    "(`parked_bikes`, `timestamp`, `station_id`) "
-    "VALUES (%(parked_bikes)s, %(timestamp)s, %(station_id)s)"
+    "INSERT INTO station "
+    "(`id`, `name`, `capacity`) "
+    "VALUES (%(id)s, %(name)s, %(capacity)s)"
 )
 
 my_db = mysql.connector.connect(
@@ -20,7 +20,6 @@ my_db = mysql.connector.connect(
 )
 my_cursor = my_db.cursor()
 
-# ele_data = {}
 
 url = 'slovnaftbajk.sk/en/stations'
 req = requests.get("http://" + url)
@@ -32,15 +31,13 @@ for table in soup.find_all('tr', {'class', 'trStation'}):
     timestamp = datetime.datetime.now()
     station_id = int(ele[1].text)
 
-    # ele_data[station_id] = [ele[2].text, []]
-    # ele_data[station_id][1].append((ele[3].text, timestamp.strftime("%Y-%m-%d %H:%M:%S")))
-
     # new data for bikes table
     data_bikes = {
-      'parked_bikes': ele[3].text,
-      'timestamp': timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-      'station_id': station_id
+        'id': station_id,
+        'name': ele[2].text,
+        'capacity': ele[3].text.split("/")[1]
     }
+    # pprint.pprint(data_bikes)
 
     my_cursor.execute(INSERT_BIKES, data_bikes)
 
@@ -48,5 +45,3 @@ my_db.commit()
 
 my_cursor.close()
 my_db.close()
-
-# pprint.pprint(ele_data)
